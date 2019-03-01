1c40fb1b2ee4f7bc5f0155241b3ac936383e055f
Student <example@example.com>
2019-03-01 15:21:54 -0800

    setup project with basic component tree

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Homepage from './Homepage';
+import Header from './Header';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route component={Error404} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
