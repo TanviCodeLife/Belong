101a2a520f089931ec4a21fcb68460949a1e49f9
Student <example@example.com>
2019-03-08 11:28:22 -0800

    add store, import reducer, provider to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { AppContainer } from 'react-hot-loader';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import { Provider } from '.react-redux';
 import { HashRouter } from 'react-router-dom';
+import { AppContainer } from 'react-hot-loader';
+
+const store = createStore(rootReducer);
+
+let unsubscribe = store.subscribe(() =>
+  console.log('subscription', store.getState())
+);
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <HashRouter>
-        <Component/>
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <Component/>
+        </HashRouter>
+      </Provider>
     </AppContainer>,
     document.getElementById('react-app-root')
   );
