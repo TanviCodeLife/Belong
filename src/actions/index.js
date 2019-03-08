56bcbafc2077a43569d12b3d463d5dc36dc65e64
Student <example@example.com>
2019-03-08 12:06:27 -0800

    add middleware logger and fetchCoords action for geocoding API call

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,15 @@ export const requestCoords = ({ lat, lng }) => ({
   lat: lat,
   lng: lng
 })
+
+export function fetchCoords(address){
+  return function(dispatch){
+    return fetch('https://maps.googleapis.com/maps/api/geocode/json?address='+address+'&key='+process.env.GOOGLE_MAPS_API)
+    .then((response) => response.json(),
+    error => console.log('An error occured', error))
+    .then((json) => {
+      const newCoords = json.results[0].geometry.location;
+      dispatch(requestCoords(newCoords));
+    });
+  };
+}
