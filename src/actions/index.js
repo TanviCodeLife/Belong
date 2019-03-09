ff9f9440fb873460711f31ddcf484800739e6399
Student <example@example.com>
2019-03-08 16:29:54 -0800

    combine api call to fetch neighborhoods into the getCoords api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,14 @@ export const requestCoords = ({ lat, lng }) => ({
   lng: lng
 })
 
+
+export const saveHoods = (hoodName, hoodlat , hoodlng) => ({
+  type: types.SAVE_HOODS,
+    hoodName: hoodName,
+    hoodLat: hoodLat,
+    hoodLng: hoodLng
+});
+
 export function fetchCoords(address){
   return function(dispatch){
     return fetch('https://maps.googleapis.com/maps/api/geocode/json?address='+address+'&key='+process.env.GOOGLE_API_KEY)
@@ -15,7 +23,23 @@ export function fetchCoords(address){
     .then((json) => {
       const newCoords = json.results[0].geometry.location;
       console.log(newCoords);
-      dispatch(requestCoords(newCoords));
+      fetchNeighborhoods(newCoords, dispatch);
     });
   };
+}
+
+export function fetchNeighborhoods(newCoords, dispatch){
+  console.log(newCoords);
+  return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?location=${ newCoords.lat }, ${ newCoords.lng }&radius=5000&type=neighborhood&key=${ process.env.GOOGLE_API_KEY }`)
+  .then((response) => response.json(),
+  error => console.log('An error occurred.', error))
+  .then(function(json) {
+    if(json.results) {
+      console.log('Neighborhoods', json.results[1].name);
+      console.log('Neighborhoods', json.results[2].name);
+    } else {
+      console.log('No neighborhoods found');
+    }
+    dispatch(requestCoords(newCoords));
+  });
 }
