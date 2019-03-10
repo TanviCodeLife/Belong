2757baa0515f85a17af3ce26ebaf80c40b5aa7f2
Tanvi Garg <tanvi.garg23@gmail.com>
2019-03-09 22:25:08 -0800

    add action type and action for fetch neighborhoods api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,11 +8,12 @@ export const requestCoords = ({ lat, lng }) => ({
 })
 
 
-export const saveHoods = (hoodName, hoodlat , hoodlng) => ({
-  type: types.SAVE_HOODS,
+export const saveHoods = (hoodName, hoodLat , hoodLng, hoodId) => ({
+  type: types.SAVE_HOOD,
     hoodName: hoodName,
     hoodLat: hoodLat,
-    hoodLng: hoodLng
+    hoodLng: hoodLng,
+    hoodId: hoodId
 });
 
 export function fetchCoords(address){
@@ -29,17 +30,23 @@ export function fetchCoords(address){
 }
 
 export function fetchNeighborhoods(newCoords, dispatch){
-  console.log(newCoords);
-  return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?location=${ newCoords.lat }, ${ newCoords.lng }&radius=5000&type=neighborhood&key=${ process.env.GOOGLE_API_KEY }`)
-  .then((response) => response.json(),
+  return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?location=${ newCoords.lat }, ${ newCoords.lng }&radius=5000&type=neighborhood&key=${ process.env.GOOGLE_API_KEY }`).then((response) => response.json(),
   error => console.log('An error occurred.', error))
   .then(function(json) {
     if(json.results) {
-      console.log('Neighborhoods', json.results[1].name);
-      console.log('Neighborhoods', json.results[2].name);
+      console.log('hoods', json.results);
+      json.results.forEach(hood => {
+        const displayName = hood.displayName;
+        const hoodLat = hood.location.lat;
+        const hoodLng = hood.location.lng;
+        const hoodId = v4();
+        console.log(displayName);
+        dispatch(saveHoods(displayName, hoodLat, hoodLng, hoodId));
+      })
     } else {
       console.log('No neighborhoods found');
     }
     dispatch(requestCoords(newCoords));
+
   });
 }
--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,1 +1,2 @@
 export const GET_COORDS = 'GET_COORDS';
+export const SAVE_HOOD = 'SAVE_HOOD';
