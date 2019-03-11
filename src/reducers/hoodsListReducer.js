14ebda976986589d3a36a5a9486940264f1f16d4
Tanvi Garg <tanvi.garg23@gmail.com>
2019-03-11 08:29:22 -0700

    add hoodsListReducer and tests

--- src/reducers/hoodsListReducer.js
+++ src/reducers/hoodsListReducer.js
@@ -1,7 +1,22 @@
+import * as types from './../constants/ActionTypes';
+
 export default (state = {}, action) => {
-  // let newSate;
-  // const { displayName, hoodLat, hoodLng, id } = action;
+  let newSate;
+  const { displayName, hoodLat, hoodLng, id } = action;
   switch(action.type) {
+    case 'SAVE_HOOD':
+      newState = Object.assign({}, state, {
+        [id]:{
+          displayName: displayName,
+          hoodLat: hoodLat,
+          hooLng: hoodLng,
+          id: id,
+          commuteTime: 0,
+          rentalRate: 0,
+          buyRate: 0
+        }
+      });
+      return newState;
     default:
     return state;
   }
